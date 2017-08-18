24b99fe18342bb7c3ed1786f11a1197c82cc8b97
Sumeet <sumeetsukhija@gmail.com>
2017-08-18 16:34:56 +1000

    Adding delete functionality

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,31 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to the MongoDB server');
+  }
+  console.log('Connection successful to MongoDB');
+
+  //deleteMany
+  // db.collection('Todos').deleteMany({text:'Eat Lunch'}).then((result) =>  {
+  //   console.log(result);
+  // });
+
+
+  //deleteOne
+  // db.collection('Todos').deleteOne({text:'Eat Lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  //findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // db.collection('Users').deleteMany({name: 'Sumeet'});
+
+  db.collection('Users').findOneAndDelete({name: 'Mike'});
+
+//  db.close();
+});
