2780e50ac746717644e05d76730edd256e30d0d7
Sumeet <sumeetsukhija@gmail.com>
2017-09-09 17:55:05 +1000

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -24,6 +25,26 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    console.log('Invalid id specified');
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (todo) {
+      res.send({todo});
+    }
+    else {
+      console.log('Id not found');
+      res.status(404).send();
+    }
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
