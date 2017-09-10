3593272dd3c70ccf2fdd36c6eae46ad48f8645c4
Sumeet <sumeetsukhija@gmail.com>
2017-09-10 20:48:05 +1000

    Added test cases for single returns of todo items

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,14 +1,17 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
-}]
+}];
 
 beforeEach((done) => {
   Todo.remove({}).then(() => {
@@ -68,4 +71,30 @@ describe('GET /todos', () => {
       })
       .end(done);
   })
-})
+});
+
+describe('GET /todos/:id', () => {
+  it('should get specific todo', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return a 404 if todo not found', (done) => {
+    var _id = new ObjectID();
+    request(app)
+      .get(`/todos/${_id}`)
+      .expect(404)
+      .end(done);
+  });
+  it('should return a 404 for non-object ids', (done) => {
+    request(app)
+      .get(`/todos/123`)
+      .expect(404)
+      .end(done);
+  });
+});
