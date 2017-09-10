13d24a8ca90e163e9b4b7f2a45bcfe472ca02784
Sumeet <sumeetsukhija@gmail.com>
2017-09-10 21:47:14 +1000

    Added update method to nodes todo api

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -64,6 +65,34 @@ app.delete('/todos/:id', (req, res) => {
     else {
       console.log('Id not found');
       res.status(404).send();
+    }
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    console.log('Invalid id specified');
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      console.log('Id not found');
+      res.status(404).send();
+    } else {
+        res.send({todo});
     }
   }).catch((e) => {
     res.status(400).send();
